2607cd7ea9721448fba4c12bbecdc5f8c9360eb2
Andy Medlicott <amedlicott11@gmail.com>
2020-05-19 15:41:04 +0100

    Fixed problem with calling getenv

--- entrypoint.py
+++ entrypoint.py
@@ -4,7 +4,7 @@ import yaml
 import os
 from pathlib import Path
 
-config_file = os.getenv["INPUT_CONFIG_FILE", "config.yaml"]
+config_file = os.getenv("INPUT_CONFIG_FILE", "config.yaml")
 
 with open(f"/github/workspace/{config_file}", "r") as stream:
     config = yaml.safe_load(stream)
