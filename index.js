6c508ed6be1cebf4482c9bb03b3faf2a089abea5
Jon Moss <jon.moss@konsentus.com>
2020-08-16 14:36:09 +0100

    👨‍🔧 Added logging, should be replica of python action

---
+++ index.js
@@ -0,0 +1,55 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+/**
+ * Reads a yaml file into a JS object.
+ *
+ * @param {str} configFileLocation - The location of the yaml config file.
+ * @returns {Object}
+ */
+const readYamlFile = (configFileName) => {
+  try {
+    return yaml.safeLoad(fs.readFileSync(`/github/workspace/${configFileName}`, 'utf8'));
+  } catch (e) {
+    core.setFailed(`File ${configFile} cannot be read: ${err}`);
+  }
+};
+
+const run = async () => {
+  core.info('Starting action.set-env');
+  try {
+    const configFileName = process.env.INPUT_CONFIG_FILE;
+    const env = process.env.APP_ENV || github.context.ref.replace('refs/heads/', '').split('/')[0];
+    const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
+
+    core.info(`configFileName: ${configFileName}`);
+    core.info(`env: ${env}`);
+    core.info(`repoName: ${repoName}`);
+
+    core.exportVariable(REPO_NAME, repoName);
+    core.exportVariable(REPO_NAME_DASH, repo_name.replace('.', '-'));
+
+    const config = readYamlFile(configFileName);
+
+    if (!config['default']) {
+      core.setFailed('Default configuration cannot be found, aborting...');
+    }
+
+    if (!config[env]) {
+      core.warning(`No config explicitly defined for environment ${env}, using default`);
+    }
+
+    completeConfig = { ...config['default'], ...config[env] };
+
+    Object.entries(completeConfig).forEach(([key, value]) => {
+      core.exportVariable(key, value);
+    });
+    core.info('Completed action.set-env');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
