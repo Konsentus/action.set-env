d71d9d9de268fedc7293747ad9e7db9feb2383a4
Andy Medlicott <amedlicott11@gmail.com>
2020-11-03 15:41:36 +0000

    Ensure extra newline is not added

--- entrypoint.py
+++ entrypoint.py
@@ -30,7 +30,7 @@ with open(os.environ["GITHUB_ENV"], "a") as environment_file:
 
     for k, v in mergedConfig.items():
         if "\n" in str(v):
-            environment_file.write(f"{k.upper()}<<delimiter\n{v}\ndelimiter\n")
+            environment_file.write(f"{k.upper()}<<delimiter\n{v}delimiter\n")
         else:
             environment_file.write(f"{k.upper()}={v}\n")
 
